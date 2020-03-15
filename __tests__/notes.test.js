485b49b09612181a8e293868a1721a829cc2c193
Earl Jay Caoile <unfie555@gmail.com>
2020-03-15 14:12:46 -0700

    100% test coverage

--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -1,16 +1,28 @@
 const notesHandler = require('../lib/notes.js');
 
+beforeEach(() => {
+  jest.spyOn(global.console, 'log');
+  console.log = jest.fn();
+});
+
 describe('notesHandler', () => {
   it('console logs', () => {
-    jest.spyOn(global.console, 'log');
-    console.log = jest.fn();
-
     const inputObj = {
       action: 'add',
-      payload: 'I like green eggs and ham!'
+      payload: 'I like green eggs and ham!',
     };
 
     notesHandler(inputObj);
     expect(console.log).toHaveBeenCalled();
+  });
+
+  it('does not console log for a non-add', () => {
+    const inputObj = {
+      action: 'subtract',
+      payload: 'I like green eggs and ham!',
+    };
+
+    notesHandler(inputObj);
+    expect(console.log).not.toHaveBeenCalled();
   });
 });
